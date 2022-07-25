cdf50c19d9c171bac20716f9b203197c8a13732a
Sumit Kumar Mishra <86769736+coderskm@users.noreply.github.com>
2022-07-25 15:20:08 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const route = require("./routes/route.js");
 const mongoose = require("mongoose");
+const multer = require("multer");
 const app = express();
+const uploadImage = multer();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(uploadImage.any());
 
 mongoose
   .connect("mongodb+srv://sumitkm:<password>@cluster0.tuxaaky.mongodb.net/group62Database")
