28a4e89296b2188d5f5fbe4f049461bdffb11eba
Sumit Kumar Mishra <86769736+coderskm@users.noreply.github.com>
2022-07-25 15:04:37 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,1 +1,12 @@
-//route
+const express = require("express");
+const router = express.Router();
+const {registerUser} = require('../controllers/userController')
+
+router.post("/register", registerUser); 
+ 
+
+router.all("*", function (req, res) {
+  return res.status(400).send({ status: false, message: "invalid URL" });
+});
+
+module.exports = router;
