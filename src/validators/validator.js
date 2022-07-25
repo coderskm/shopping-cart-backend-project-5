6960fe684f2a1ecd9ead6c5e1dcc14693cb9d59b
Sumit Kumar Mishra <86769736+coderskm@users.noreply.github.com>
2022-07-25 15:05:15 +0530

    Update validator.js

--- src/validators/validator.js
+++ src/validators/validator.js
@@ -2,4 +2,21 @@ const isValidBody = (value) => {
     return Object.keys(value).length > 0;
 }
 
-module.exports = {isValidBody}
+const isValid =  (value)=> {
+  if (typeof value === "undefined" || value === null) return false;
+  if (typeof value === "string" && value.trim().length === 0) return false;
+  if (typeof value === "number") return false;
+  return true;
+};
+
+const isValidPassword = (password) => {
+    if (password.length >= 8 && password.length <= 15)
+        return true;
+}
+
+const isValidFiles = (files) => {
+  if (files && files.length > 0) {
+    return true;
+  }
+}
+module.exports = { isValidBody, isValid, isValidPassword, isValidFiles };
