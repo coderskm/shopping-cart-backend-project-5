ac7c01d8433c2cc03b9b083dd2d448474ea5f044
Namrata jaiswal <namrata.jaiswal102441@gmail.com>
2022-08-03 17:44:24 +0530

    almost Done

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -8,21 +8,50 @@ const orderModel=require('../models/orderModel');
 const placeOrder=async function(req,res){
     try{
        let userId=req.params.userId;
-       let  {cartId,cancellable ,status,items:[productId,quantity]} = data
-       const cartCheck=await cartModel.findOne({userId})
-       if(!cartCheck){res.status(404).send({status:false,message:"No cart found for this user"})}
 
-       const productCheck=await productModel.findOne({productId});
-       if(!productCheck){res.status(404).send({status:false,message:"No such product"})}
-
+       
        const userCheck=await userModel.find({userId});
        if(!userCheck){return res.status(404).send({status:false,message:"No such user exists"})}
 
-conosle.log(cartCheck)
+       let cartId = req.body.cartId
+       const cartCheck=await cartModel.findById(cartId)
+       if(!cartCheck){res.status(404).send({status:false,message:"No cart found for this user"})}
+       if(cartCheck.items.length == 0) {
+          return res.status(400).send({status:false, message:"you can't order anything from the cart"})
+       }
+       let total =0
+        cartCheck.items.forEach(ele => total += ele.quantity)
+       let CreateOder = {};
+       CreateOder.userId = userId;
+       CreateOder.totalItems = cartCheck.totalItems;
+       CreateOder.totalPrice =  cartCheck.totalPrice;
+       CreateOder.items = cartCheck.items
+       CreateOder.totalQuantity = total
+       CreateOder.status = "pending"
 
-       const orderData=await orderModel.create(data)
-       return res.status(201).send({status:true,message:"order placed successfully"})
+       const orderData=await orderModel.create(CreateOder)
+       return res.status(201).send({status:true,message:"order placed successfully" , data: orderData})
 
     }catch(err){res.status(500).send({status:false,message:err.message})}
 }
-module.exports={placeOrder}
+
+const updateOrder=async function(req,res){
+    try{
+       let userId=req.params.userId;
+        let {orderId} = req.body
+       
+       const userCheck=await userModel.findOne({userId});
+       if(!userCheck){return res.status(404).send({status:false,message:"No such user exists"})}
+
+       const checkOder=await orderModel.findById(orderId)
+       if(!checkOder){return  res.status(404).send({status:false,message:"No Oder found for this user"})}
+
+    //    if(checkOder.cancellable == false) {return res.status(400).send({status:false, message:"this oder can't be cancled"})}
+       
+
+       const orderData=await orderModel.findOneAndUpdate(orderId,{ status:"completed"},{new:true})
+       return res.status(201).send({status:true,message:"order completed successfully" , data: orderData})
+
+    }catch(err){res.status(500).send({status:false,message:err.message})}
+}
+module.exports={placeOrder,updateOrder}
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const { authentication ,authorisation} = require("../Auth/userAuth");
-const {registerUser,login, getProfile,updateUser} = require('../controllers/userController')
-const{newProduct,getAllProduct,getSingleProduct,updateProduct,deleteProduct}=require("../controllers/productController")
-const{cartCreation, getCart, updateCart, deleteCart}=require('../controllers/cartController')
-const {placeOrder}=require('../controllers/orderControler')
+const { authentication, authorisation } = require("../Auth/userAuth");
+const { registerUser, login, getProfile, updateUser } = require('../controllers/userController')
+const { newProduct, getAllProduct, getSingleProduct, updateProduct, deleteProduct } = require("../controllers/productController")
+const { cartCreation, getCart, updateCart, deleteCart } = require('../controllers/cartController')
+const { placeOrder, updateOrder } = require('../controllers/orderController')
 
 
 /* USER API's */
-router.post("/register", registerUser); 
-router.post("/login", login); 
+router.post("/register", registerUser);
+router.post("/login", login);
 router.get("/user/:userId/profile", authentication, getProfile);
-router.put("/user/:userId/profile", authentication,authorisation, updateUser);
+router.put("/user/:userId/profile", authentication, authorisation, updateUser);
 
 /* PRODUCT API's */
 router.post("/products", newProduct);
@@ -22,13 +22,14 @@ router.put("/products/:productId", updateProduct);
 router.delete("/products/:productId", deleteProduct);
 
 /* CART APIs */
-router.post( "/users/:userId/cart",authentication,authorisation, cartCreation); 
+router.post("/users/:userId/cart", authentication, authorisation, cartCreation);
 router.get("/users/:userId/cart", authentication, authorisation, getCart);
 router.put("/users/:userId/cart", authentication, authorisation, updateCart);
 router.delete("/users/:userId/cart", authentication, authorisation, deleteCart);
 
 /*ORDER APIs */
-router.post("/users/:userId/orders",placeOrder)
+router.post("/users/:userId/orders", placeOrder)
+router.put("/users/:userId/orders", updateOrder)
 
 /*BAD URL */
 router.all("*", function (req, res) {
